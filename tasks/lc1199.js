764f4c62b4da7aeaa2e956be77a68eca48762ed1
Lc1199 <2226435862@qq.com>
2018-03-14 02:53:30 +0800

    finsh

---
+++ tasks/lc1199.js
@@ -0,0 +1,79 @@
+/*
+ * grunt-lc1199
+ * 
+ *
+ * Copyright (c) 2018 Lc1199
+ * Licensed under the MIT license.
+ */
+
+'use strict';
+
+var path = require('path');
+
+module.exports = function (grunt) {
+
+  // Please see the Grunt documentation for more information regarding task
+  // creation: http://gruntjs.com/creating-tasks
+
+  grunt.registerMultiTask('lc1199', 'just demo', function () {
+
+    // Merge task-specific and/or target-specific options with these defaults.
+    var options = this.options({
+      who: 'book',
+      commentSymbol: '//'
+    });
+
+    var testExistRegexMap = {
+      'book': /============/,
+      'alpaca': /┃┫┫　┃┫┫/
+    }
+
+    var who = options.who,
+        commentSymbol = options.commentSymbol,
+
+        commentFilepathMap = {
+          'lc1199': 'assets/alpaca.txt',
+          'book': 'assets/book.txt'
+        },
+        commentFilepath = path.join(__dirname , commentFilepathMap[who]),
+        commentContent = grunt.file.read(commentFilepath),
+
+        lineCommentArr = commentContent.split(grunt.util.normalizelf('\n'));
+
+    lineCommentArr.forEach(function(value , index , arr){
+      arr[index] = commentSymbol + value;
+    });
+
+    commentContent = lineCommentArr.join(grunt.util.normalizelf('\n'));
+
+    // Iterate over all specified file groups.
+    this.files.forEach(function (file) {
+      // Concat specified files.
+      file.src.filter(function (filepath) {
+        // Warn on and remove invalid source files (if nonull was set).
+        if (!grunt.file.exists(filepath)) {
+          grunt.log.warn('Source file "' + filepath + '" not found.');
+          return false;
+        } else {
+          return true;
+        }
+      }).map(function (filepath) {
+        // Read file source.
+        var originalFileContent = grunt.file.read(filepath),
+            newFileContent = commentContent + 
+                              grunt.util.normalizelf('\n') + 
+                              originalFileContent;
+
+        if(originalFileContent.split(';')[0].length > 0){
+          return;
+        }
+
+        grunt.file.write(filepath , newFileContent);
+      });
+
+      // Print a success message.
+      grunt.log.writeln('File "' + file.dest + '" created.');
+    });
+  });
+
+};
